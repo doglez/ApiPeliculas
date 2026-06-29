552bd0fe426f4afbc5e8476bd2cb448a665c9f05
danilo.gonzalez.ardon <danilo.gonzalez.ardon@outlook.com>
2026-06-29 09:23:18 -0600

    Modelo Category

---
+++ ApiPeliculas/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
